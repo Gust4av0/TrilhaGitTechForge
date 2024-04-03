3006fe1a0556d8cafd378a40520dccae09b82981
gustavosoares2024 <gustavo.soares@grupointegrado.br>
2024-04-03 20:19:12 -0300

    Criando Exercício 2

---
+++ ex2.js
@@ -0,0 +1,11 @@
+function verificaNumero(numero) {
+    if (numero % 2 === 0) {
+        console.log("É par");
+    } else {
+        console.log("É Ímpar");
+    }
+}
+
+//Testando
+verificaNumero(4);
+verificaNumero(7);
